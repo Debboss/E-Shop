d37747bf89dc6aaec106ebfef46a4bd3c5c6b979
Thanos Afentras <aafentras@nyc.gr>
2024-03-03 16:42:41 +0200

    made the tabs for the navbar, added a cart icon for the user's cart and connected all the files. I will style it as we go.

--- eshop/market.py
+++ eshop/market.py
@@ -12,5 +12,17 @@ def home_page():
 def products_page():
     return render_template('products.html', item_name = 'Phone')
 
+@app.route('/contact_us')
+def contactUs_page():
+    return render_template('contact_us.html')
+
+@app.route('/subscription_plans')
+def subscriptionPlans_page():
+    return render_template('subscription_plans.html')
+
+@app.route('/cart')
+def cart_page():
+    return render_template('cart.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
